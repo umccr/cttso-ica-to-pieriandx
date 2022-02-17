2a6b53c6826c253773368fa2f10e20dd0bfc2f02
Alexis Lucattini <alexis.lucattini@umccr.org>
2022-02-17 13:58:17 +1100

    Removed commas from if command so if - fi is all on one line

--- deploy/stacks/pipeline_stack.py
+++ deploy/stacks/pipeline_stack.py
@@ -224,8 +224,8 @@ class PipelineStack(Stack):
                         # Build as latest tag
                         "docker build --tag \"${CONTAINER_REPO}/${CONTAINER_NAME}:latest\" ./",
                         # Also add in tag if applicable
-                        "if [ -n \"${CTTSO_ICA_TO_PIERIANDX_GIT_TAG}\" ]; then",
-                        "  docker tag \"${CONTAINER_REPO}/${CONTAINER_NAME}:latest\" \"${CONTAINER_REPO}/${CONTAINER_NAME}:${CTTSO_ICA_TO_PIERIANDX_GIT_TAG}\""
+                        "if [ -n \"${CTTSO_ICA_TO_PIERIANDX_GIT_TAG-}\" ]; then "
+                        "  docker tag \"${CONTAINER_REPO}/${CONTAINER_NAME}:latest\" \"${CONTAINER_REPO}/${CONTAINER_NAME}:${CTTSO_ICA_TO_PIERIANDX_GIT_TAG}\" " 
                         "fi",
                         "export \"CTTSO_ICA_TO_PIERIANDX_GIT_TAG\"",
                         "echo \"Container version is ${CTTSO_ICA_TO_PIERIANDX_GIT_TAG-latest}\" 1>&2"
@@ -236,8 +236,8 @@ class PipelineStack(Stack):
                         # Login to aws and push Docker image to ECR
                         "aws ecr get-login-password --region \"${REGION}\" | docker login --username AWS --password-stdin \"${CONTAINER_REPO}\"",
                         "docker push \"${CONTAINER_REPO}/${CONTAINER_NAME}:latest\"",
-                        "if [ -n \"${CTTSO_ICA_TO_PIERIANDX_GIT_TAG}\" ]; then",
-                        "  docker push \"${CONTAINER_REPO}/${CONTAINER_NAME}:${CTTSO_ICA_TO_PIERIANDX_GIT_TAG}\"",
+                        "if [ -n \"${CTTSO_ICA_TO_PIERIANDX_GIT_TAG-}\" ]; then  "
+                        "  docker push \"${CONTAINER_REPO}/${CONTAINER_NAME}:${CTTSO_ICA_TO_PIERIANDX_GIT_TAG}\"  "
                         "fi"
                     ]
                 }
