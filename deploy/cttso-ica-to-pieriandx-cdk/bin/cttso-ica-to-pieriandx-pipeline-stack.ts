d11283a52a52a9032ed7543f1ccb9e4aaac3922b
Alexis Lucattini <alexis.lucattini@umccr.org>
2022-07-08 21:05:35 +1000

    Add prod stack

--- deploy/cttso-ica-to-pieriandx-cdk/bin/cttso-ica-to-pieriandx-pipeline-stack.ts
+++ deploy/cttso-ica-to-pieriandx-cdk/bin/cttso-ica-to-pieriandx-pipeline-stack.ts
@@ -5,9 +5,12 @@ import { CttsoIcaToPieriandxPipelineStack } from '../lib/cttso-ica-to-pieriandx-
 import {
     AWS_BUILD_ACCOUNT_ID,
     AWS_DEV_ACCOUNT_ID,
+    AWS_PROD_ACCOUNT_ID,
     AWS_REGION,
     DEV_STACK_SUFFIX,
+    PROD_STACK_SUFFIX,
     GITHUB_DEV_BRANCH_NAME,
+    GITHUB_PROD_BRANCH_NAME,
     STACK_PREFIX
 } from "../constants";
 
@@ -24,4 +27,16 @@ const dev_stack = new CttsoIcaToPieriandxPipelineStack(app, 'CttsoIcaToPieriandx
         region: AWS_REGION
     },
     stack_suffix: DEV_STACK_SUFFIX
+});
+
+const prod_stack = new CttsoIcaToPieriandxPipelineStack(app, 'CttsoIcaToPieriandxPipelineStackProd', {
+    stack_prefix: `${STACK_PREFIX}-${PROD_STACK_SUFFIX}`,
+    aws_region: AWS_REGION,
+    aws_account_id: AWS_PROD_ACCOUNT_ID,
+    github_branch_name: GITHUB_PROD_BRANCH_NAME,
+    env: {
+        account: AWS_BUILD_ACCOUNT_ID,
+        region: AWS_REGION
+    },
+    stack_suffix: PROD_STACK_SUFFIX
 });
