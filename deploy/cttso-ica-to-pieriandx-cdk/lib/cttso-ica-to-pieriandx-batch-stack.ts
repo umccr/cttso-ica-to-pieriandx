97602a0950de92851ba471ecadd634594c635205
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2022-09-20 18:21:38 +1000

    Allow two cpus as max vcpus

--- deploy/cttso-ica-to-pieriandx-cdk/lib/cttso-ica-to-pieriandx-batch-stack.ts
+++ deploy/cttso-ica-to-pieriandx-cdk/lib/cttso-ica-to-pieriandx-batch-stack.ts
@@ -353,7 +353,7 @@ export class CttsoIcaToPieriandxBatchStack extends Stack {
                     type: ComputeResourceType.ON_DEMAND,
                     allocationStrategy: AllocationStrategy.BEST_FIT_PROGRESSIVE,
                     desiredvCpus: 0,
-                    maxvCpus: 1,
+                    maxvCpus: 2,
                     minvCpus: 0,
                     image: MachineImage.genericLinux(
                         {
