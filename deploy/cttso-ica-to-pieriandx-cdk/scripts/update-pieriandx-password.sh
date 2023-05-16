e897d76cc3e50b26110e8857679d3a63bf23d8f3
Alexis Lucattini <alexis.lucattini@umccr.org>
2023-05-16 17:58:48 +1000

    Updated password script to ensure that generated token is valid
    Also updated Readme to ask user to not use special characters in password.  
    
    Added Keybase step too.

--- deploy/cttso-ica-to-pieriandx-cdk/scripts/update-pieriandx-password.sh
+++ deploy/cttso-ica-to-pieriandx-cdk/scripts/update-pieriandx-password.sh
@@ -9,13 +9,24 @@ set -euo pipefail
 # Globals
 SECRET_ID="PierianDx/UserPassword"
 
+# Glocals
+env_midfix=""
+institution=""
+base_url=""
+
 # Get json file based on account
 account_id="$(aws sts get-caller-identity --output json | jq --raw-output '.Account')"
 
 if [[ "${account_id}" == "843407916570" ]]; then
   echo "Updating password in dev" 1>&2
+  env_midfix="dev"
+  institution="melbournetest"
+  base_url="https://app.uat.pieriandx.com/cgw-api/v2.0.0"
 elif [[ "${account_id}" == "472057503814" ]]; then
   echo "Updating password in prod" 1>&2
+  env_midfix="prod"
+  institution="melbourne"
+  base_url="https://app.pieriandx.com/cgw-api/v2.0.0"
 else
   echo "Please ensure you're logged in to either dev or prod" 1>&2
   exit 1
@@ -40,6 +51,50 @@ input_secret_json_str="$( \
 )"
 
 echo "Updating secret in ${account_id}" 1>&2
-aws secretsmanager update-secret \
-  --secret-id "${SECRET_ID}" \
-  --secret-string "${input_secret_json_str}"
+secret_version_id="$( \
+  aws secretsmanager update-secret \
+    --output json \
+    --secret-id "${SECRET_ID}" \
+    --secret-string "${input_secret_json_str}" | \
+  jq --raw-output ".VersionId" \
+)"
+
+echo "Got secret version id ${secret_version_id}" 1>&2
+
+echo "Sleeping 5 for secret to be fully updated" 1>&2
+sleep 5
+
+echo "Running lambda to update token" 1>&2
+aws lambda invoke \
+  --output json \
+  --function-name "cttso-ica-to-pieriandx-${env_midfix}-token-refresh-lambda-stack-lf" \
+  /dev/null > /dev/null
+
+echo "Sleep 5 for token to be fully updated" 1>&2
+sleep 5
+
+echo "Collecting token from aws secretsmanager" 1>&2
+new_auth_token="$( \
+  aws secretsmanager get-secret-value \
+    --secret-id PierianDx/UserAuthToken \
+    --output json | \
+  jq --raw-output \
+   '.SecretString | fromjson | .PierianDxUserAuthToken'
+)"
+
+echo "Listing All Cases" 1>&2
+if [[ "$( \
+  curl \
+    --fail --silent --location --show-error \
+    --request "GET" \
+    --url "${base_url}/case" \
+    --header "Accept: application/json" \
+    --header "X-Auth-Email: services@umccr.org" \
+    --header "X-Auth-Token: $new_auth_token" \
+    --header "X-Auth-Institution: $institution" | \
+  jq 'length > 0' \
+)" == "true" ]]; then
+  echo "Password changed successfully!" 1>&2
+else
+  echo "Error! Password changed however was not able to use generated token" 1>&2
+fi
